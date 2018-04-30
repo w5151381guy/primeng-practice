cd0fb9000672083fd4fa8e85a62df6e28f1dcc93
AndyChen <w5151381guy@gmail.com>
2018-04-30 17:29:14 +0800

    finish picklist

---
+++ picklist/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core'
+
+interface Source {
+  brand?
+  year?
+  color?
+}
+
+@Component({
+  selector: 'picklist',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  sourceItems: Source[]
+
+  targetItems: Source[]
+
+  constructor() {
+    this.sourceItems = [
+      { brand: 'VW', year: 2012, color: 'Orange' },
+      { brand: 'Audi', year: 2011, color: 'Black' },
+      { brand: 'Renault', year: 2005, color: 'Gray' },
+      { brand: 'BMW', year: 2003, color: 'Blue' },
+      { brand: 'Mercedes', year: 1995, color: 'Orange' },
+      { brand: 'Volvo', year: 2005, color: 'Black' },
+      { brand: 'Honda', year: 2012, color: 'Yellow' },
+      { brand: 'Jaguar', year: 2013, color: 'Orange' },
+      { brand: 'Ford', year: 2000, color: 'Black' },
+      { brand: 'Fiat', year: 2013, color: 'Red' },
+    ]
+  }
+
+  ngOnInit() {
+    this.targetItems = []
+  }
+}
